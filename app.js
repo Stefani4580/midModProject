c55cdf127e7f80f5859e81aa027940e8b0813350
Kingzz <krem@Kingzzs-MBP.fios-router.home>
2020-07-03 13:53:09 -0400

    js commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// I will be using querySelector to return the element that mataches a css selector
+// using constant to declare variables su to there is no mutuation will be taken.
+
+const actionDiv = document.querySelector('.container-action');
+const resetDiv = document.querySelector('.restart');
+const boxDivs = document.querySelectorAll('.game-box')
+
+
+
+let gameLetsAGo = true;
+let marioTurn = true;
